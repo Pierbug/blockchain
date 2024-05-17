8cd966f47a4fe506b3f484078a1ae1b26cad7ed2
alessiozanettii <alessioz2005@gmail.com>
2024-05-17 11:16:04 +0200

    main

---
+++ main.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BlockChain</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>BlockChain</h1>
+    <div class="button-container">
+        <button class="red-button">Aggiungi Blocco</button>
+        <button class="red-button">Mostra Tutti i Blocchi</button>
+    </div>
+</body>
+</html>
